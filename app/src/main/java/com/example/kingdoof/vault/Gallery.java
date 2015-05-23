1633b62c9e5fafcaebe3351dcd22da6a5c012997
Ryan Newell <thekingdoof@gmail.com>
2015-05-23 15:07:10 -0500

    Added button and text box

--- app/src/main/java/com/example/kingdoof/vault/Gallery.java
+++ app/src/main/java/com/example/kingdoof/vault/Gallery.java
@@ -1,40 +1,23 @@
 package com.example.kingdoof.vault;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 
-public class Main extends ActionBarActivity {
-
-    private EditText editTextPassword;
-    private Button enterButton;
+public class Gallery extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        enterButton = (Button) findViewById(R.id.enterButton);
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Button Pressed", Toast.LENGTH_LONG).show();
-                
-            }
-        });
-
+        setContentView(R.layout.activity_gallery);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_gallery, menu);
         return true;
     }
 
